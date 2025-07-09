ce0d14f77461910fac8da951ff265a55e0671ea3
Arofat Nizamova <arofatnizamova@gmail.com>
2025-07-09 22:25:10 +0500

    feat: add logout

--- Todo-list/src/views/WelcomeView.vue
+++ Todo-list/src/views/WelcomeView.vue
@@ -2,9 +2,20 @@
     <div class="text-center">
       <h1>Добро пожаловать!</h1>
       <p>Вы зарегистрированы и вошли в систему.</p>
+      <button @click="logout" class="btn btn-outline-danger">
+      Выйти
+    </button>
     </div>
   </template>
   
   <script setup>
+    import { useRouter } from 'vue-router';
+    
+    const router = useRouter();
+    
+    const logout = () => {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user_id');
+      router.push('/login');
+    };
   </script>
-  
